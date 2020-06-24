1f43b41b1d54cd1a4ab8e3835811331fcf73c07f
Deepak <deepakpra2@gmail.com>
2020-06-24 14:54:32 +0530

    Updates

---
+++ precache-manifest.c1573059af9e8de64d6b4f7853156047.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da907aa275bbf07fcf47f1e97227f1cf",
+    "url": "/React-clock/index.html"
+  },
+  {
+    "revision": "82179b3870af31d3499f",
+    "url": "/React-clock/static/js/2.e346f9ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-clock/static/js/2.e346f9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea71d00f446b9daec19",
+    "url": "/React-clock/static/js/main.49a11766.chunk.js"
+  },
+  {
+    "revision": "458c755d9b5112bbf491",
+    "url": "/React-clock/static/js/runtime-main.ec3a500a.js"
+  }
+]);
